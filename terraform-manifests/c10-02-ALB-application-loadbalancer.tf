e82d350236139d1c5fb24dc3854337e58cf42c08
rgupta1996-6 <rajatsinghal996@gmail.com>
2021-10-13 23:44:43 +0530

    updates

--- terraform-manifests/c10-02-ALB-application-loadbalancer.tf
+++ terraform-manifests/c10-02-ALB-application-loadbalancer.tf
@@ -2,7 +2,7 @@
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   #version = "5.16.0"
-  version = "6.0.0" 
+  version = "6.5.0"
 
   name = "${local.name}-alb"
   load_balancer_type = "application"
