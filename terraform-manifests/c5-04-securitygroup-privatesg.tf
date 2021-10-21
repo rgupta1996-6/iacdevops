8e0482bc0963b7c78485a2cda93561c984592724
rgupta1996-6 <rajatsinghal996@gmail.com>
2021-10-21 14:28:21 +0530

    updates

--- terraform-manifests/c5-04-securitygroup-privatesg.tf
+++ terraform-manifests/c5-04-securitygroup-privatesg.tf
@@ -10,10 +10,26 @@ module "private_sg" {
   description = "Security Group with HTTP & SSH port open for entire VPC Block (IPv4 CIDR), egress ports are all world open"
   vpc_id = module.vpc.vpc_id
   # Ingress Rules & CIDR Blocks
-  ingress_rules = ["ssh-tcp", "http-80-tcp", "http-8080-tcp","http-9935-tcp","http-9945-tcp"]
+  ingress_rules = ["ssh-tcp", "http-80-tcp", "http-8080-tcp"]
   ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
   # Egress Rule - all-all open
   egress_rules = ["all-all"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 9935
+      to_port     = 9935
+      protocol    = 6
+      description = "Allow Port 9935 from internet"
+      cidr_blocks = "10.0.0.0/16"
+    },
+      {
+      from_port   = 9945
+      to_port     = 9945
+      protocol    = 6
+      description = "Allow Port 9945 from internet"
+      cidr_blocks = "10.0.0.0/16"
+    },
+  ]
   tags = local.common_tags
 }
 
