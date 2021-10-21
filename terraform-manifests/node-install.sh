a1cb6e7c85fa6bdb312065a691fd101cd81b4f99
rgupta1996-6 <rajatsinghal996@gmail.com>
2021-10-21 17:33:09 +0530

    updates

--- terraform-manifests/node-install.sh
+++ terraform-manifests/node-install.sh
@@ -6,3 +6,4 @@ cd substrate-as/
 nohup ./vtb-node --base-path /tmp/vtb-node/temp02 --chain local --name hodl-validator-node1  --charlie --port 30337 --ws-port 9947 --rpc-port 9937 --validator --offchain-worker Never >> /home/ubuntu/substrate-as/node1.log 2>&1 &
 sleep 2
 grep identity node1.log | awk -F': ' '{print $2}' > demo.log
+hostname -I | awk '{print $1}' > node1-ip.log
